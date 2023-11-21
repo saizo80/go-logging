0f212cc429dde7a487603d0e9337a5dc8e9d3dc4
sai <saizo@simplemail.is>
2023-11-21 12:09:14 -0600

    do not print date to stdout

--- logging.go
+++ logging.go
@@ -57,7 +57,7 @@ func (l *Logger) print(level string, message interface{}, color string, args ...
 		color,
 		level,
 		reset,
-		now.Format("2006-01-02 15:04:05,000"),
+		now.Format("15:04:05,000"),
 		message,
 	)
 }
